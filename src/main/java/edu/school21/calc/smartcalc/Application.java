0a09e1f0d13c84e58d14018fd27e748ce231ea5a
achanel <x@x>
2023-10-22 15:52:33 +0300

    start

---
+++ src/main/java/edu/school21/calc/smartcalc/Application.java
@@ -0,0 +1,13 @@
+package edu.school21.calc.smartcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
